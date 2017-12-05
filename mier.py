d53751856c16dfefe1fee55ede0ce9e7e8b6c41f
DutchGhost <kasper199914@gmail.com>
2017-12-05 13:13:03 +0100

    beide dingetjes

---
+++ mier.py
@@ -0,0 +1,107 @@
+from functools import partial
+from itertools import islice
+
+'''ITERATOR FUNCTIONS'''
+#returns a chunk of the iterable
+def take(n, iterable):
+    return list(islice(iterable, n))
+
+#returns a chucked iterator over the iterable
+def chunk(iterable, n):
+    return iter(partial(take, n, iter(iterable)), [])
+'''END OF ITERATOR FUNCTIONS'''
+
+'''Stringify a nested list.
+   after each row yield an '\n', but only if it's not the last row
+'''
+def to_string(iterable, positional = " "):
+    last = len(iterable) - 1
+    for (idx, row) in enumerate(iterable):
+        for item in row:
+            yield ''.join([item, positional])
+        if idx != last:
+            yield "\n"
+        else:
+            break
+
+def tekst(matrix):
+    return ''.join(to_string(matrix))
+
+"""First do 2 asserts.
+One to check if the input is squarable (n * n should be equal to the lenght of 'configuratie'),
+and another to check if there's no other character than '^', '>', 'v' or '<'.
+"""
+def rooster(n, configuratie):
+    valids = ['^', '>', 'v', '<']
+    
+    assert(n * n == len(configuratie)), "ongeldige argumenten"
+    assert(all(map(lambda token: token in valids, configuratie))), "ongeldige argumenten"
+    
+    return list(chunk(configuratie, n))
+
+"""Walls are only important if the next step would be in the wall.
+so if the field is '<', and the x = 0...we return the the wall is "LEFT".
+MAX_X and MAX_Y are the maximum x and y values in the matrix.
+"""
+def type_wall(x, y, field, MAX_X, MAX_Y):
+    if field == '<' and x == 0: return "LEFT"
+    elif field == '^' and y == 0: return "UPPER"
+    elif field == '>' and x == MAX_X: return "RIGHT"
+    elif field == 'v' and y == MAX_Y: return "BOTTEM"
+    else: return "NONE"
+
+def turn_field(field):
+    if field == '^': return '>'
+    if field == '>': return 'v'
+    if field == 'v': return '<'
+    if field == '<': return '^'
+
+"""ONLY perform a step if we can.
+   NOTE:
+        with the y axis, we subtract 1 if the wall is NOT 'UPPER' and the field is '^',
+        and we add 1 if the wall is NOT 'BOTTEM' and the field is 'v'
+        this is becouse the higher we go in the matrix, the lesser the y value becomes.
+"""
+def nxt(type_wall, current_x, current_y, field):
+    return \
+    {
+        '^': lambda wall: (current_y, current_x) if wall == 'UPPER' else (current_y - 1, current_x),
+        '>': lambda wall: (current_y, current_x) if wall == 'RIGHT' else (current_y, current_x + 1),
+        'v': lambda wall: (current_y, current_x) if wall == 'BOTTEM' else (current_y + 1, current_x),
+        '<': lambda wall: (current_y, current_x) if wall == 'LEFT' else (current_y, current_x - 1),
+    }[field](type_wall)
+
+def stap(matrix, y_x):
+    (y, x) = y_x
+    MAX_X, MAX_Y = (len(matrix) - 1, len(matrix) - 1)
+    for (current_y, row) in enumerate(matrix):
+        for (current_x, field) in enumerate(row):
+            if (current_x, current_y) == (x, y):
+                wall = type_wall(current_x, current_y, field, MAX_X, MAX_Y)            
+                matrix[current_y][current_x] = turn_field(field)
+                return nxt(wall, current_x, current_y, field)
+
+"""first yield current.
+Then, for as long as the end is not reached, set a step, and yield that step.
+"""
+def stepstream(matrix, start, end):
+    current = start
+    yield current
+    while current != end:
+        current = stap(matrix, current)
+        yield current
+
+
+def stappen(matrix):
+    start = (len(matrix) - 1, 0)
+    stop = (0, len(matrix) - 1)
+    return list(stepstream(matrix, start, stop))
+
+if __name__ == '__main__':
+    vierkant = rooster(4, ">>>>^<^v^v^^>>v>")
+    
+    print(tekst(vierkant))
+    t = stappen(vierkant)
+    print(t)
+
+    #print(tekst(vierkant))
